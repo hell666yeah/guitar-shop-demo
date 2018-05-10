182b56014a52c0227db93ca8933540e717a3c2cf
Nikhil Chandran <p.nikhil.chandran@gmail.com>
2018-05-10 22:42:38 +0530

    Get straps product list api

--- src/app/straps-listing/straps-listing.component.ts
+++ src/app/straps-listing/straps-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-straps-listing',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./straps-listing.component.scss']
 })
 export class StrapsListingComponent implements OnInit {
+  public productsList: Array<any>;
+  public filteredProductsList: Array<any>;
+  public filters: Array<string> = ['straps', 'small', 'large'];
 
-  constructor() { }
+  constructor(private http: Http) {this.http.post('http://127.0.0.1:3000/straps', {}).subscribe(data => {
+      this.productsList = data.json();
+      this.filteredProductsList = this.productsList;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  public filterChanged(filter) {
+    let filteredProducts = [];
+    console.log(filter, 'filter');
+    if (filter === 'straps') {
+        this.filteredProductsList = this.productsList;
+    } else {
+      this.productsList.forEach(product => {
+          if (product.size === filter) {
+              filteredProducts.push(product);
+          }
+      });
+
+      this.filteredProductsList = filteredProducts;
+    }
   }
 
 }
