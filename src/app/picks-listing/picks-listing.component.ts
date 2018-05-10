e98b9dda9ae4be034cebd244af54ae5e5a44dca8
Nikhil Chandran <p.nikhil.chandran@gmail.com>
2018-05-10 22:43:02 +0530

    Get picks list API

--- src/app/picks-listing/picks-listing.component.ts
+++ src/app/picks-listing/picks-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-picks-listing',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./picks-listing.component.scss']
 })
 export class PicksListingComponent implements OnInit {
+  public productsList: Array<any>;
+  public filteredProductsList: Array<any>;
+  public filters: Array<string> = ['picks', 'plastic', 'wooden'];
 
-  constructor() { }
+  constructor(private http: Http) {this.http.post('http://127.0.0.1:3000/picks', {}).subscribe(data => {
+      this.productsList = data.json();
+      this.filteredProductsList = this.productsList;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  public filterChanged(filter) {
+    let filteredProducts = [];
+    console.log(filter, 'filter');
+    if (filter === 'picks') {
+        this.filteredProductsList = this.productsList;
+    } else {
+        this.productsList.forEach(product => {
+            if (product.subCategory === filter) {
+                filteredProducts.push(product);
+            }
+        });
+
+        this.filteredProductsList = filteredProducts;
+    }
   }
 
 }
